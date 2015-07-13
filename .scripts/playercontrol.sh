e4be7333cbc34e297e2031bf0ad1f1ae79ff2f8d
Bruno93PT <bruno.fl.jesus@gmail.com>
2015-07-13 21:41:34 +0100

    update

--- .scripts/playercontrol.sh
+++ .scripts/playercontrol.sh
@@ -3,7 +3,7 @@
 if [ $1 == "next" ] ; then
 	playerctl next
 	mpc next
-elif [ $1 == "previous "] ; then
+elif [ $1 == "previous" ] ; then
 	playerctl previous
 	mpc prev
 elif [ $1 == "toggle" ] ; then
